af63242501cc99c0f29b90aad3c4d48c39b63694
Errazorr <56584432+Errazorr@users.noreply.github.com>
2020-03-16 12:59:55 +0100

    Création de la méthode pour l'inscription

--- Manager/Connexion.php
+++ Manager/Connexion.php
@@ -28,4 +28,33 @@ class methode{
       echo '<meta http-equiv="refresh" content="0;URL=../View/Connexion.php">';
     }
   }
+
+
+
+  public function inscription(user $inscription){
+    try{
+      $bdd= new PDO('mysql:host=localhost;dbname=cine; charset=utf8','root','');
+    }
+    catch (Exception $e){
+      die('Erreur:'.$e->getMessage());
+    }
+
+    $req = $bdd->prepare('SELECT * FROM compte WHERE mail=?');
+    $req->execute(array($connexion->getMail()));
+    $donnees= $req->fetch();
+
+    if ($donnees) {
+      echo '<body onLoad="alert(\'Utilisateur déjà existant\')">';
+
+      echo '<meta http-equiv="refresh" content="0;URL=../View/Inscription.php">';
+    }
+
+    else {
+      $req = $bdd->prepare('INSERT INTO compte (nom, prenom, mail, tel, mdp, role) VALUES (?,?,?,?,?,?)');
+      $req->execute(array($inscription->getNom(), $inscription->getPrenom(), $inscription->getMail(), $inscription->getTel(), md5($inscription->getMdp()), 'client'));
+      $_SESSION['nom'] = $inscription->getNom();
+      $_SESSION['prenom'] = $inscription->getPrenom();
+    }
+
+  }
 }
