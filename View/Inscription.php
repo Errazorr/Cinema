9ea5d0e6fb764cd77ec46c105e863087ae8f9b82
Errazorr <56584432+Errazorr@users.noreply.github.com>
2020-03-16 13:43:02 +0100

    Modification de la vue du formulaire d'inscription

--- View/Inscription.php
+++ View/Inscription.php
@@ -28,17 +28,26 @@
                                 <input type="text" name="nom" id="name" placeholder="Votre nom"/>
                             </div>
                             <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="prenom" id="surname" placeholder="Votre prénom"/>
+                            </div>
+                            <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
                                 <input type="email" name="email" id="email" placeholder="Votre email"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input type="tel" name="telephone" id="re_pass" placeholder="Numero de telephone" maxlength="10" minlength="10"/>
                             </div>
                             <div class="form-group">
                                 <label for="pass"><i class="zmdi zmdi-lock"></i></label>
                                 <input type="password" name="mdp" id="pass" placeholder="Mot de passe"/>
                             </div>
                             <div class="form-group">
-                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                <input type="tel" name="telephone" id="re_pass" placeholder="Numero de telephone" maxlength="10" minlength="10"/>
+                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="confirm" id="pass" placeholder="Confirmer le mot de passe"/>
                             </div>
+
                             <div class="form-group">
                                 <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
                                 <label for="agree-term" class="label-agree-term"><span><span></span></span>j'accepte les <a href="#" class="term-service">termes et services</a></label>
