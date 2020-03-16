6f370d32af1e39a0d8faff3999e85ba74973ce10
Errazorr <56584432+Errazorr@users.noreply.github.com>
2020-03-16 13:40:09 +0100

    Traitement pour s'inscrire

---
+++ Traitement/Inscription.php
@@ -0,0 +1,25 @@
+<?php
+
+require '../Model/user.php';
+require '../Manager/Methodes.php';
+session_start();
+
+if ($_POST['mdp'] != $_POST['confirm']) {
+  echo '<body onLoad="alert(\'Les deux mots de passe sont différents\')">';
+
+  echo '<meta http-equiv="refresh" content="0;URL=../View/inscription.php">';
+}
+
+else{
+  $inscription = new user(['nom' =>$_POST['nom'],
+                                  'prenom' =>$_POST['prenom'],
+                                  'id' =>$_POST['id'],
+                                  'mail' =>$_POST['mail'],
+                                  'tel' =>$_POST['tel'],
+                                  'mdp' =>$_POST['mdp']]);
+  $inscrit = new methode;
+  $inscrit->inscription($inscription);
+}
+
+
+?>
