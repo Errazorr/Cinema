600ed043c7779e1005575b39cc117f4e6ccdf50b
Yanish-bjn <56584457+Yanish-bjn@users.noreply.github.com>
2020-03-30 10:21:41 +0200

    Correcton d'une erreur au niveau de l'insertion BDD pour la page contact

--- Manager/Contact.php
+++ Manager/Contact.php
@@ -18,36 +18,37 @@ public function contact($donnee){
       $bdd=new PDO('mysql:host=localhost;dbname=cine; charset=utf8','root','');
     $req=$bdd->prepare('INSERT into contact (nom, mail, message) VALUES(:nom, :mail, :message)');
     $req->execute(array('nom'=>$donnee->getnom(), 'mail'=>$donnee->getmail(), 'message'=>$donnee->getmessage()));
+    $a=$req->fetchall();
     if ($req ==true){
       header("location: ../index.php");
     }
     else{
       header("location: ../View/contact.php");
     }
-          }
+  }
 
-public function Mail($donnee){
-$mail = new PHPMailer();
-$mail->isSMTP();                                            // Send using SMTP
-$mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-$mail->Username   = 'iliasverif@gmail.com';                     // SMTP username
-$mail->Password   = 'Tamere95';                               // SMTP password
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-$mail->Port       = 587;                                    // TCP port to connect to
+  public function Mail($donnee){
+  $mail = new PHPMailer();
+  $mail->isSMTP();                                            // Send using SMTP
+  $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+  $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+  $mail->Username   = 'iliasverif@gmail.com';                     // SMTP username
+  $mail->Password   = 'Tamere95';                               // SMTP password
+  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+  $mail->Port       = 587;                                    // TCP port to connect to
 
-//Recipients
-$mail->setFrom('iliasverif@gmail.com', 'Info');
-$mail->addAddress($donnee->getmail(), 'Contact');     // Add a recipient //Recipients
- $mail->Body    =   $donnee->getmessage();
-if(!$mail->Send()) {
-  echo '<body onLoad="alert(\'Erreur\')">';
-echo '<meta http-equiv="refresh" content="0;URL=../View/contact.php">';
-} else {
-   header("location: ../index.php");
-}
+  //Recipients
+  $mail->setFrom('iliasverif@gmail.com', 'Info');
+  $mail->addAddress($donnee->getmail(), 'Contact');     // Add a recipient //Recipients
+   $mail->Body    =   $donnee->getmessage();
+  if(!$mail->Send()) {
+    echo '<body onLoad="alert(\'Erreur\')">';
+  echo '<meta http-equiv="refresh" content="0;URL=../View/contact.php">';
+  } else {
+     header("location: ../index.php");
+  }
 
-    }
+      }
 }
 
 ?>
