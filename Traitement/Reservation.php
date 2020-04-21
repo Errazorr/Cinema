7815a70b43ebdfb1ff531c745e450e75981fe401
Errazorr <56584432+Errazorr@users.noreply.github.com>
2020-04-21 09:42:00 +0200

    Traitement de la réservation

--- Traitement/Reservation.php
+++ Traitement/Reservation.php
@@ -1,25 +1,16 @@
 <?php
 session_start();
-require '../Manager/Methodes.php';
-require '../Model/Reservation.php';
+require_once '../Manager/Methodes.php';
+require_once '../Model/Reservation.php';
 
-if ($_POST['mdp'] != $_POST['confirm']) {
-  echo '<body onLoad="alert(\'Les deux mots de passe sont différents\')">';
-
-  echo '<meta http-equiv="refresh" content="0;URL=../View/inscription.php">';
-}
-
-else{
-  $reservation = new reservation(['nom' =>$_SESSION['nom'],
-                                  'tel' =>$_POST['telephone'],
-                                  'film' =>$_POST['film'],
-                                  'date' =>$_POST['date'],
-                                  'adulte' =>$_POST['adulte'],
-                                  'ado' =>$_POST['ado'],
-                                  'enfant' =>$_POST['enfant'],]);
-  $reserver = new methode;
-  $reserver->reservation($reservation);
-}
-
+$reservation = new reservation(['nom' =>$_SESSION['nom'],
+                                'tel' =>$_SESSION['tel'],
+                                'film' =>$_POST['film'],
+                                'date' =>$_POST['date'],
+                                'adulte' =>$_POST['adulte'],
+                                'ado' =>$_POST['ado'],
+                                'enfant' =>$_POST['enfant'],]);
+$reserver = new methode;
+$reserver->reservation($reservation);
 
 ?>
