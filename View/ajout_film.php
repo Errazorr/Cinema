bf283f1709d4972e45d98b08976114bf357caab1
Errazorr <56584432+Errazorr@users.noreply.github.com>
2020-05-10 13:06:02 +0200

    ajout d'un film

--- View/ajout_film.php
+++ View/ajout_film.php
@@ -1,121 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Hazze Template">
-  <meta name="keywords" content="Hazze, unica, creative, html">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Ajouter un film</title>
 
-  <!-- Css Styles -->
-  <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
-  <link rel="stylesheet" href="../css/font-awesome.min.css" type="text/css">
-  <link rel="stylesheet" href="../css/themify-icons.css" type="text/css">
-  <link rel="stylesheet" href="../css/owl.carousel.min.css" type="text/css">
-  <link rel="stylesheet" href="../css/magnific-popup.css" type="text/css">
-  <link rel="stylesheet" href="../css/slicknav.min.css" type="text/css">
-  <link rel="stylesheet" href="../css/style.css" type="text/css">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>Commentaires</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+
+	<!-- Bootstrap -->
+	<link type="text/css" rel="stylesheet" href="../reservation/css/bootstrap.min.css" />
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="../reservation/css/style.css" />
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
 </head>
 
 <body>
-  <!-- Header Section Begin -->
-  <header class="header-section">
-      <div class="container">
-          <div class="row">
-              <div class="col-lg-2 col-md-2">
-                  <div class="logo">
-                      <a href="#">
-                          <img src="../img/logo.png" alt="">
-                      </a>
-                  </div>
-              </div>
-              <div class="col-lg-10 col-md-10">
-                  <div class="main-menu mobile-menu">
-                    <ul>
-                      <li class="active"><a href="../index.php">Accueil</a></li>
-                      <li><a href="Nouveaute.php">Film & évènement</a>
-                        <ul class="dropdown">
-                          <li><a href="Nouveaute.php">Nouveauté</a></li>
-                          <li><a href="Prochainement.php">Prochainement</a></li>
-                        </ul>
-                      </li>
-                      <li><a href="ajout_film.php">Ajouter un film</a></li>
-                      <li><a href="changer_salle.php">Changer un film de salle</a></li>
-                      <li><a href="gestion_clients.php">Gestion des utilisateurs</a></li>
-                      <li><a href="voir_reservation.php">Gestion des réservations</a></li>
-                      <li><a href="Ajout_admin.php">Ajouter un administrateur</a></li>
-                      <li><a href="../Traitement/session_destroy.php">Déconnexion</a></li>
-                    </ul>
-                  </div>
-              </div>
-          </div>
-          <div id="mobile-menu-wrap"></div>
-      </div>
-  </header>
-</body>
+	<?php
+	try{
+		$bdd= new PDO('mysql:host=localhost;dbname=cine; charset=utf8','root','');
+	}
+	catch (Exception $e){
+		die('Erreur:'.$e->getMessage());
+	}
+	?>
+	<div id="booking" class="section">
+		<div class="section-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-7 col-md-push-5">
+						<div class="booking-cta">
+							<h1>Commentaires</h1>
+							<p>Laissez donc un petit commentaire sur le film que vous venez de voir. Faites nous savoir ce que vous en avez pensé.
+							</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-md-pull-7">
+						<div class="booking-form">
+							<form method="POST" action="../Traitement/commentaires.php">
+								<div class="form-group">
+									<span class="form-label">Choisir le film</span>
+									<select class="form-control" name="film" placeholder="Choisissez un film">
 
-<!-- Footer Section Begin -->
-<section class="footer-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-6">
-                <div class="footer-option">
-                    <div class="fo-logo">
-                        <a href="#">
-                            <img src="../img/logo.png" alt="">
-                        </a>
-                    </div>
-                    <ul>
-                        <li>Adresse: Rue de Rivoli, 75001 Paris</li>
-                        <li>Phone: +33 07 00 00 00 00</li>
-                        <li>Email: Palais_du_cinema@gmail.com</li>
-                    </ul>
-                    <div class="fo-social">
-                        <a href="https://fr-fr.facebook.com/"><i class="fa fa-facebook"></i></a>
-                        <a href="https://www.instagram.com/?hl=fr"><i class="fa fa-instagram"></i></a>
-                        <a href="https://twitter.com/login?lang=fr"><i class="fa fa-twitter"></i></a>
-                        <a href="https://www.pinterest.fr/"><i class="fa fa-pinterest"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="footer-widget fw-links">
-                    <h5>informations</h5>
-                    <ul>
-                        <li><a href="reservation.php">Reservation</a></li>
-                        <li><a href="contact.php">Contact</a></li>
-                        <li><a href="Connexion.php">Connexion</a></li>
-                        <li><a href="Nouveaute.php">Nouveauté</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="footer-widget">
-                    <h5>Rejoinez la newsletter</h5>
-                    <p>Vous devez être inscrit pour pouvoir vous inscrire à la newsletter.</p>
-                    <form method="post" action="../Manager/newsletter.php" class="news-form">
-                        <input type="text" name="mail" placeholder="Enter votre email">
-                        <button type="submit">Enregistrer</button>
-                    </form>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="footer-widget">
-                    <h5>Instagram</h5>
-                    <div class="insta-pic">
-                        <img src="../img/instagram/instagram-1.jpg" alt="">
-                        <img src="../img/instagram/instagram-2.jpg" alt="">
-                        <img src="../img/instagram/instagram-3.jpg" alt="">
-                        <img src="../img/instagram/instagram-4.jpg" alt="">
-                        <img src="../img/instagram/instagram-5.jpg" alt="">
-                        <img src="../img/instagram/instagram-6.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="copyright-text"><p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p></div>
-    </div>
-</section>
+											<?php
+											$req = $bdd->query('SELECT film FROM salle');
+									    $donnees= $req->fetchall();
+
+											foreach ($donnees as $value) {
+												echo '<option>'.$value["film"].'</option>';
+											}
+											?>
+
+									</select>
+								</div>
+								<div class="row">
+									<div class="col-sm-6">
+										<div class="form-group">
+											<span class="form-label">Entrez votre commentaire</span>
+											<input class="form-control" type="textarea" name="commentaire" placeholder="Votre commentaire" required>
+										</div>
+									</div>
+								</div>
+								<div class="form-btn">
+									<button class="submit-btn">Laisser un commentaire</button>
+									<button class="submit-btn" onclick="window.location.href='compte_client.php'">Retour</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
