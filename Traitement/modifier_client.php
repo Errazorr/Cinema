6a3fce7731d163a64f4323d72b7d0133e5bb4ebb
Errazorr <56584432+Errazorr@users.noreply.github.com>
2020-05-11 09:17:39 +0200

    Commentaires traitement

--- Traitement/modifier_client.php
+++ Traitement/modifier_client.php
@@ -1,13 +1,14 @@
 <?php
-
+//Evoie des données vers les pages suivantes //
 require '../Model/user.php';
 require '../Manager/Methodes.php';
 
+//Enregistrement des données //
 $modification_client = new user(['nom' =>$_POST['nom'],
                         'prenom' =>$_POST['prenom'],
                         'mail' =>$_POST['email'],
                         'tel' =>$_POST['telephone']]);
-$modif_client = new methode;
+$modif_client = new methode;//Déclration d'une nouvelle methode //
 $modif_client->modif_client($modification_client);
 
 
--- Traitement/supprimer_client.php
+++ Traitement/supprimer_client.php
@@ -1,11 +1,13 @@
 <?php
+// Envoie des données vers les pages suivantes //
 require_once('../Manager/Methodes.php');
 require_once('../Model/Reservation.php');
 
+//Enregistrement des données //
 $suppr_client = new user(['nom' =>$_POST['nom'],
                           'prenom' =>$_POST['prenom']]);
 
-$supprimer_client = new methode;
+$supprimer_client = new methode; //Déclration d'une nouvelle methode //
 $supprimer_client->suppr_client($suppr_client);
 
 ?>
