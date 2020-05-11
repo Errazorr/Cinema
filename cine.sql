9c84be38819a88aedff8747fc5e6d4591cafc551
Errazorr <56584432+Errazorr@users.noreply.github.com>
2020-05-11 11:13:28 +0200

    BDD

--- cine.sql
+++ cine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 10 mai 2020 à 10:24
+-- Généré le :  lun. 11 mai 2020 à 09:13
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
   `com` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fkfilm` (`film`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `commentaires`
@@ -46,7 +46,8 @@ INSERT INTO `commentaires` (`id`, `nom`, `film`, `com`) VALUES
 (1, 'aa', 'Sonic le film', 'Un excellent film, une belle animation, un bon sens de l\'humour'),
 (2, 'test', 'Sonic le film', 'Très bon film, adapté pour tous, hâte de voir si il sort en 3D'),
 (3, 'aa', 'En Avant Disney', 'Film très mignon, adapté pour tout fan de Disney'),
-(4, 'aa', 'L\'appel de la foret', 'Un film intéressant, j\'ai beaucoup aimé l\'histoire. Je le conseille vivement');
+(4, 'aa', 'L\'appel de la foret', 'Un film intéressant, j\'ai beaucoup aimé l\'histoire. Je le conseille vivement'),
+(5, 'aa', 'Le Voyage de Chihiro', 'Un film passionnant et pleins d\'émotions');
 
 -- --------------------------------------------------------
 
@@ -66,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `compte` (
   PRIMARY KEY (`id`),
   KEY `nom` (`nom`) USING BTREE,
   KEY `tel` (`tel`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `compte`
@@ -74,8 +75,8 @@ CREATE TABLE IF NOT EXISTS `compte` (
 
 INSERT INTO `compte` (`id`, `nom`, `prenom`, `mail`, `tel`, `mdp`, `role`) VALUES
 (1, 'test', 'test', 'test@test.fr', '0612345678', '098f6bcd4621d373cade4e832627b4f6', 'admin'),
-(2, 'aa', 'aa', 'aa@gmail.com', '0655441122', '4124bc0a9335c27f086f24ba207a4912', 'client'),
-(3, 'test2', 'test2', 'test2@test2.fr', '0987654321', 'ad0234829205b9033196ba818f7a872b', 'admin');
+(2, 'aa', 'bb', 'aa@gmail.com', '0655441122', '4124bc0a9335c27f086f24ba207a4912', 'client'),
+(4, 'test2', 'test2', 'test2@test2.fr', '0987654321', 'ad0234829205b9033196ba818f7a872b', 'admin');
 
 -- --------------------------------------------------------
 
@@ -119,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   KEY `fk_nom` (`nom`),
   KEY `fk_tel` (`tel`),
   KEY `fk_num_salle` (`num_salle`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `reservation`
@@ -128,8 +129,8 @@ CREATE TABLE IF NOT EXISTS `reservation` (
 INSERT INTO `reservation` (`id`, `nom`, `tel`, `num_salle`, `prix`, `nb_pers`, `date_prevue`, `reservation`) VALUES
 (2, 'aa', '0655441122', 2, 40, 4, '2020-04-29', 'aa/Sonic le film/2020-04-29'),
 (3, 'aa', '0655441122', 6, 48, 5, '2020-05-11', 'aa/En Avant Disney/2020-05-11'),
-(4, 'test', '0612345678', 2, 56, 6, '2020-04-28', 'test/Sonice le film/2020-04-28'),
-(5, 'aa', '0655441122', 1, 62, 6, '2020-05-11', 'test/L\'appel de la foret/2020-05-11');
+(5, 'aa', '0655441122', 9, 62, 6, '2020-05-11', 'aa/L\'appel de la foret/2020-05-11'),
+(6, 'aa', '0655441122', 3, 38, 4, '2020-05-16', 'aa/Le Voyage de Chihiro/2020-05-16');
 
 -- --------------------------------------------------------
 
@@ -149,17 +150,18 @@ CREATE TABLE IF NOT EXISTS `salle` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `film` (`film`),
   KEY `num` (`num`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `salle`
 --
 
 INSERT INTO `salle` (`id`, `num`, `film`, `description`, `3D`, `nb_places`, `places_restantes`) VALUES
-(1, 1, 'L\'appel de la foret', 'Le chien Buck vivait depuis quatre ans dans la famille du juge Miller, au Sud des États-Unis, quand il s\'est vu impliqué, malgré lui, dans l\'aventure de la ruée vers l\'or du Nord.', 'non', 50, 2),
+(1, 9, 'L\'appel de la foret', 'Le chien Buck vivait depuis quatre ans dans la famille du juge Miller, au Sud des États-Unis, quand il s\'est vu impliqué, malgré lui, dans l\'aventure de la ruée vers l\'or du Nord.', 'non', 50, 2),
 (2, 2, 'Sonic le film', 'Sonic et Tom unissent leurs forces pour tenter d\'empêcher le terrible Dr. Robotnik de capturer Sonic, ce dernier souhaitant utiliser son immense pouvoir pour dominer le monde.', 'non', 100, 90),
 (3, 5, 'De Gaulle', 'La guerre s\'intensifie, l\'armée française s\'effondre, les Allemands seront bientôt à Paris. La panique gagne le gouvernement qui envisage d\'accepter la défaite. Un homme, Charles de Gaulle, fraîchement promu général, veut infléchir le cours de l\'Histoire.', 'non', 50, 50),
-(4, 6, 'En Avant Disney', 'Dans la banlieue d\'un univers imaginaire, deux frères elfes se lancent dans une quête extraordinaire pour découvrir s\'il reste encore un peu de magie dans le monde.', 'oui', 70, 65);
+(4, 6, 'En Avant Disney', 'Dans la banlieue d\'un univers imaginaire, deux frères elfes se lancent dans une quête extraordinaire pour découvrir s\'il reste encore un peu de magie dans le monde.', 'oui', 70, 65),
+(5, 3, 'Le Voyage de Chihiro', 'Le film raconte l\'histoire de Chihiro, une fillette de dix ans qui, alors qu\'elle se rend en famille vers sa nouvelle maison, entre dans le monde des esprits. Après la transformation de ses parents en porcs par la sorcière Yubaba, Chihiro prend un emploi dans l\'établissement de bains de Yubaba pour retrouver ses parents et le monde humain.', 'Non', 100, 76);
 
 --
 -- Contraintes pour les tables déchargées
@@ -175,7 +177,7 @@ ALTER TABLE `commentaires`
 -- Contraintes pour la table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD CONSTRAINT `fk_nom` FOREIGN KEY (`nom`) REFERENCES `compte` (`nom`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_nom` FOREIGN KEY (`nom`) REFERENCES `compte` (`nom`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_num_salle` FOREIGN KEY (`num_salle`) REFERENCES `salle` (`num`) ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_tel` FOREIGN KEY (`tel`) REFERENCES `compte` (`tel`) ON UPDATE CASCADE;
 COMMIT;
